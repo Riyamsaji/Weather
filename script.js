b02ab2575b87760d4776beeeb769bdec000cd3df
riya <riyamanalil@gmail.com>
2021-07-11 23:03:49 +0530

    update

--- script.js
+++ script.js
@@ -12,55 +12,48 @@ function displayDetails(city) {
     let sealevel = city.main.sea_level;
     let latitude = city.coord.lat;
     let longitude = city.coord.lon;
+    console.log(iconid);
     let html_data = `<div class="container-fluid mt-5">
+   
     <div class="row ">
-        <div class="card" style="height: 600px;">
-            <div class="card-body bg-light">
+       <div class="card" style="height: 600px;color:#0f2862">
+        <h1 class= "text-center text-uppercase" style="font-size:70px"> ${city_name}</h1>
+       
+            <div class="card-body">
                 <div class="row mt-5">
-                <div class="col-lg-3 col-md-4 col-6" style="margin-top:20px">
-                <label class=" text-capitalize text-left" style="font-size:20px">Description<b> ${weather_des}</b></label>
-                </div>
-                <div class="col-lg-3 col-md-4 col-6"style="margin-top:20px">
-                    <label style="font-size:30px"><b>${temp}C°</b></label>
-                </div>
-                <div class="col-lg-3 col-md-4 col-6 "style="margin-top:20px">
-                <label class=" text-capitalize text-right">City<b> ${city_name}</b></label>
-                </div>
-                <div class="col-lg-3 col-md-4 col-6 "style="margin-top:20px"
-                    <label class=" text-capitalize text-right">Country<b> ${country}</b></label>
-                </div>
-              
-            
-            
-                <div class="col-lg-3 col-md-4 col-6"style="margin-top:20px">
-                    <label class=" text-capitalize text-right">Time-Zone:<b> ${timezone}</b></label>
-                </div>
-                <div class="col-lg-3 col-md-4 col-6"style="margin-top:20px">
-                    <label class=" text-capitalize text-right">Temp-min:<b> ${temp_min}</b></label>
-                </div>
-                <div class="col-lg-3 col-md-4 col-6"style="margin-top:20px">
-                    <label class=" text-capitalize text-right">Temp-max:<b> ${temp_max}</b></label>
+                <div class="col-lg-8 col-md-4 col-6">
+                <label style="font-size:60px"><b>${temp}C°</b></label><br>
+                <label class=" text-capitalize text-left" style="font-size:20px"><b> ${weather_des}</b></label>
                 
                 </div>
-                <div class="col-lg-3 col-md-4 col-6"style="margin-top:20px">
-                    <img src="./icons/${iconid}.png" style="width:200">
+                <div class="col-lg-4 col-md-4 col-6">
+                <label class=" text-capitalize text-right"style="font-size:60px;">Country<b> ${country}</b></label>
                 </div>
+                </div>
+                <div class="row">
+                <div class="col-lg-8 col-md-6 col-6 ">
+                <img src="./icons/${iconid}.png">
+                </div>
+                <div class="col-lg-4 col-md-6 col-6 ">
+                <label class=" text-capitalize text-right" style="font-size:20px">Time-Zone:<b> ${timezone}</b></label>
+               </div>
+               </div>
+               <div class="row">
+               <div class="col-lg-8 col-md-6 col-6">
+               <label class=" text-capitalize text-right">Temp-min:<b> ${temp_min}</b></label><br>
+               <label class=" text-capitalize text-right">Temp-max:<b> ${temp_max}</b></label>
+               </div>
+                <div class="col-lg-4 col-md-6 col-6">
+                <label class=" text-capitalize text-right">Pressure:<b> ${Pressure}</b></label><br>
+                <label class=" text-capitalize text-right">Sealevel:<b> ${sealevel}</b></label><br>
+                <label class=" text-capitalize text-right">Latitude:<b> ${latitude}</b></label><br>
+                <label class=" text-capitalize text-right">longitude:<b> ${longitude}</b></label><br>
+                </div>
+                </div>
+                   
+                
               
                
-                <div class="col-lg-3 col-md-4 col-6"style="margin-top:20px">
-                    <label class=" text-capitalize text-right">Pressure:<b> ${Pressure}</b></label>
-                </div>
-                <div class="col-lg-3 col-md-4 col-6"style="margin-top:20px">
-                    <label class=" text-capitalize text-right">Sealevel:<b> ${sealevel}</b></label>
-                </div>
-                <div class="col-lg-3 col-md-4 col-6"style="margin-top:20px">
-                    <label class=" text-capitalize text-right">Latitude:<b> ${latitude}</b></label>
-                </div>
-                <div class="col-lg-3 col-md-4 col-6"style="margin-top:20px">
-                    <label class=" text-capitalize text-right">longitude:<b> ${longitude}</b></label>
-                </div>
-              
-               </div>
                 </div>
                 </div>
                 </div>
