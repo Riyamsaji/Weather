ebbbfd8efc8ef8565aafe80aa11b2b762faaa0e3
riya <riyamanalil@gmail.com>
2021-07-12 11:17:51 +0530

    add

--- script.js
+++ script.js
@@ -13,51 +13,58 @@ function displayDetails(city) {
     let latitude = city.coord.lat;
     let longitude = city.coord.lon;
     console.log(iconid);
-    let html_data = `<div class="container-fluid mt-5">
+    let html_data = 
+    `<div class="container-fluid mt-5 section2">
    
-    <div class="row ">
-       <div class="card" style="height: 600px;color:#0f2862">
-        <h1 class= "text-center text-uppercase" style="font-size:70px"> ${city_name}</h1>
+    <div class="row mb-3 mt-3">
+    <h5 class=" text-center text-uppercase" style="font-size:70px"><u>${city_name}</u></h5>
+  <div class="col-lg-6 col-md-6 col-6">
+    <div class="sections1" >
+ 
+      <div class="sec1">
+      <img src="./icons/${iconid}.png">
+        
+      <p style="font-size:50px;"><b>${weather_des}</b></p>
+  
+      
        
-            <div class="card-body">
-                <div class="row mt-5">
-                <div class="col-lg-8 col-md-6 col-6">
-                <label style="font-size:50px"><b>${temp}C°</b></label><br>
-                <label class=" text-capitalize text-left" style="font-size:20px"><b> ${weather_des}</b></label>
-                
-                </div>
-                <div class="col-lg-4 col-md-6 col-6">
-                <label class=" text-capitalize text-right"style="font-size:50px;">Country<b> ${country}</b></label>
-                </div>
-                </div>
-                <div class="row">
-                <div class="col-lg-8 col-md-6 col-6 ">
-                <img src="./icons/${iconid}.png">
-                </div>
-                <div class="col-lg-4 col-md-6 col-6 ">
-                <label class=" text-capitalize text-right" style="font-size:20px">Time-Zone:<b> ${timezone}</b></label>
-               </div>
-               </div>
-               <div class="row">
-               <div class="col-lg-8 col-md-6 col-6">
-               <label class=" text-capitalize text-right">Temp-min:<b> ${temp_min}</b></label><br>
-               <label class=" text-capitalize text-right">Temp-max:<b> ${temp_max}</b></label>
-               </div>
-                <div class="col-lg-4 col-md-6 col-6">
-                <label class=" text-capitalize text-right">Pressure:<b> ${Pressure}</b></label><br>
-                <label class=" text-capitalize text-right">Sealevel:<b> ${sealevel}</b></label><br>
-                <label class=" text-capitalize text-right">Latitude:<b> ${latitude}</b></label><br>
-                <label class=" text-capitalize text-right">longitude:<b> ${longitude}</b></label><br>
-                </div>
-                </div>
-                   
-                
-              
-               
-                </div>
-                </div>
-                </div>
-                </div>`
+      </div>
+    </div>
+  </div>
+  <div class="col-lg-6 col-md-6 col-6">
+    <div class="sections2">
+      <div class="sec2">
+       
+        <p class=" text-center" style="font-size:50px;"><b>${temp}°C</b></p>
+        <p class="text-center">Temperature-minimum:${temp_min}</p>
+        <p class="text-center">Temperature-maximum${temp_max}</p>
+        <p class="text-center">${country}</p>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-lg-6 col-md-6" style="margin-top:20px">
+  <div class="sections3">
+  <div class="sec3">
+      <p >Pressure:${Pressure}</p>
+      <p >Latitude:${latitude}</p>
+      <p >Longitude:${longitude}</p>
+    </div>
+    </div>
+    </div>
+  <div class="col-sm-6 col-lg-6 col-md-6"style="margin-top:20px">
+  <div class="sections4">
+    <div class="sec4 text-center">
+     
+      <p class=" text-center" style="font-size:30px;"><b>HUMIDTY:${humidity}</b></p>
+      <p >SEA-LEVEL:<u><b>${sealevel}<b><u></p>
+  
+ 
+    </div>
+  </div>
+</div>
+
+</div>
+ </div>`
 
 
     result.innerHTML = html_data;
